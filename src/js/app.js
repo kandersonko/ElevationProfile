a4a21ea3034fd0a0c3f27cd38efc48891acc2e59
Koffi Anderson Koffi <kandersonko@gmail.com>
2017-04-14 16:59:09 -0700

    add highlight of selected feature on bicycle routes layer

--- src/js/app.js
+++ src/js/app.js
@@ -3,23 +3,23 @@ require([
   "esri/dijit/ElevationProfile",
   "esri/units",
   "esri/layers/FeatureLayer",
-  "esri/tasks/query",
-  "esri/tasks/QueryTask",
   "esri/dijit/Search",
-  "dojo/_base/array",
+  "esri/symbols/SimpleLineSymbol",
+  "esri/Color",
+  "esri/geometry/Polyline",
+  "esri/graphic",
   "dojo/dom",
-  "dojo/on",
   "dojo/domReady!"
 ], function (Map,
              ElevationsProfileWidget,
              Units,
              FeatureLayer,
-             Query,
-             QueryTask,
              Search,
-             array,
-             dom,
-             on) {
+             SimpleLineSymbol,
+             Color,
+             Polyline,
+             Graphic,
+             dom) {
   var map = new Map("map", {
     basemap: "streets",
     center: [-116.678, 48.097],
@@ -56,12 +56,27 @@ require([
   function init() {
     var urlParams = getJsonFromUrl();
     // OBJECTID : [1, 2, 3, 4, 8, 9];
+    // rgb(230,0,0)
     var id = urlParams.OBJECTID;
-
+    var lines = {
+      1: {color: new Color("#005CE6"), width: 12},
+      2: {color: new Color("#005CE6"), width: 12},
+      3: {color: new Color("#005CE6"), width: 12},
+      4: {color: new Color("#E60000"), width: 12},
+      8: {color: new Color("#38A800"), width: 14},
+      9: {color: new Color("#E69800"), width: 6}
+    };
+    console.log("lines: ", lines);
+    console.log("current line: ", lines[id], lines[id].color, lines[id].width);
     var search = new Search({
       map: map,
       visible: false,
       enableHighlight: true,
+      showInfoWindowOnSelect: false,
+      enableInfoWindow: false,
+      infoTemplate: null,
+      // highlightSymbol: new SimpleLineSymbol().setColor(lines[id].color).setWidth(14),
+      zoomScale: 2,
       sources: [
         {
           featureLayer: bicycleLayer,
@@ -72,14 +87,27 @@ require([
       ]
     });
 
+    // var selectionSymbol = new SimpleLineSymbol().setColor(new Color("#E69800")).setWidth(24);
+
+    var selectionSymbol = new SimpleLineSymbol().setColor(lines[id].color).setWidth(lines[id].width);
+    // bicycleLayer.setSelectionSymbol(selectionSymbol);
+
     search.startup();
+    // console.log("search: ", search);
     search.search(id).then(function (resp) {
-      var feauture = resp[0][0].feature;
-      elevationProfile.set("profileGeometry", feauture.geometry);
+      console.log("resp: ", resp);
+      var feature = resp[0][0].feature;
+      elevationProfile.set("profileGeometry", feature.geometry);
+      var polyline = new Polyline(feature.geometry);
+      var graphic = new Graphic(polyline, selectionSymbol);
+      map.graphics.add(graphic);
+      console.log("map: ", map);
     });
 
   }
 
+  // returns a json object which
+  // attributes are the url parameters
   function getJsonFromUrl() {
     var query = location.search.substr(1);
     var result = {};
