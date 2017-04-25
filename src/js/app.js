2af5737d02513de129355094b982b11d41bca194
Koffi Anderson Koffi <kandersonko@gmail.com>
2017-04-25 16:39:32 -0700

    fix tabs toggle issues

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,7 @@ require([
   "dojo/topic",
   "dojo/on",
   "dojo/dom",
+  "dojo/dom-class",
   "dojo/domReady!"
 ], function (Map,
              ElevationsProfileWidget,
@@ -27,13 +28,13 @@ require([
              Graphic,
              topic,
              on,
-             dom) {
+             dom,
+             domClass) {
   var map = new Map("map", {
     basemap: "streets",
     center: [-116.678, 48.097],
     zoom: 10
   });
-  map.on("load", init);
 
   var chartOptions = {
     titleFontColor: "#ffffff",
@@ -59,48 +60,120 @@ require([
   };
   var bicycleLayer = new FeatureLayer(bicycleServiceUrl, bicycleLayerOptions);
 
-  map.addLayer(bicycleLayer);
 
   var navigationWEServiceUrl = "http://services.arcgis.com/WLhB60Nqwp4NnHz3/arcgis/rest/services/Navigation_WE/FeatureServer/0";
   var navigationEWServiceUrl = "http://services.arcgis.com/WLhB60Nqwp4NnHz3/arcgis/rest/services/Navigation_EW/FeatureServer/0";
 
+  var tabEwDom = dom.byId('tab-east-west');
+  var tabWeDom = dom.byId('tab-west-east');
   var serviceUrlEventName = "serviceUrl:changes";
+
+  // tabEwDom.style.display = 'none';
+  tabWeDom.style.display = 'none';
+
+  map.on("load", init);
+
+  map.addLayer(bicycleLayer);
+
   on(dom.byId("EW"), 'click', function(){
-    topic.publish(serviceUrlEventName, "tab-east-west", "tab-east-east");
+    // topic.publish(serviceUrlEventName, "tab-east-west", "tab-east-east");
+    tabEwDom.style.display = 'block';
+    tabWeDom.style.display = 'none';
+    domClass.add("EW", 'selected-link');
+    domClass.remove("WE", "selected-link");
   });
 
   on(dom.byId("WE"), 'click', function(){
-    topic.publish(serviceUrlEventName, "tab-east-east", "tab-east-west");
-  });
-  
-  topic.subscribe(serviceUrlEventName, function(previous, current){
-    console.log("previous: ", previous);
-    console.log("current: ", current);
-    var previousDom = dom.byId(previous);
-    var currentDom = dom.byId(current);
-    if(previousDom !== null) {
-      previousDom.style.display = "none";
-    }
-    if(currentDom !== null) {
-      currentDom.style.display = "block";
-    }
+    // topic.publish(serviceUrlEventName, "tab-west-east", "tab-east-west");
+    tabEwDom.style.display = 'none';
+    tabWeDom.style.display = 'block';
+    domClass.remove("EW", "selected-link");
+    domClass.add("WE", "selected-link");
   });
 
+  topic.subscribe("ew:data-loaded", function (features) {
+    navigationDirections(features, "tab-east-west");
+  });
 
-  function navigationDirections(serviceUrl, id){
+  topic.subscribe("we:data-loaded", function (features) {
+    navigationDirections(features, "tab-west-east");
+  });
+
+  // topic.subscribe(serviceUrlEventName, function(previous, current){
+  // console.log("previous: ", previous);
+  // console.log("current: ", current);
+  // var previousDom = dom.byId(previous);
+  // var currentDom = dom.byId(current);
+  // if(previousDom !== null) {
+  //   previousDom.style.display = "none";
+  // }
+  // if(currentDom !== null) {
+  //   currentDom.style.display = "block";
+  // }
+  // });
+
+
+  // function navigationDirections(serviceUrl, id){
+  //   var url = "http://services.arcgis.com/WLhB60Nqwp4NnHz3/arcgis/rest/services/Navigation_EW/FeatureServer/0";
+  //   var tableDom = (serviceUrl === url) ? "tab-east-west" : "tab-west-east";
+  //   var navigationQueryTask = new QueryTask(serviceUrl);
+  //   var navigationQuery = new Query();
+  //   navigationQuery.outFields = ["*"];
+  //   navigationQuery.returnGeometry = false;
+  //   navigationQuery.multipatchOption = "xyFootprint";
+  //   navigationQuery.objectIds = [id];
+  //   // navigationQuery.where = "OBJECTID = '" + id + "'";
+  //   navigationQueryTask.on("complete", function (data) {
+  //     console.log("data: ", data);
+  //
+  //     var features = data.featureSet.features;
+  //     var table = "<table cellspacing='0' cellpadding='10'><thead><tr>" +
+  //       "<th>Images</th><th>Directions</th></tr></thead>";
+  //     table += "<tbody>";
+  //     features.forEach(function (feature) {
+  //       table += "<tr><td><img style='display:block;' width='50px' height='50px' src='" + feature.attributes.Image + "'/></td>" +
+  //         "<td>" + feature.attributes.Directions + "</td></tr>";
+  //     });
+  //
+  //     table += "</tbody></table>";
+  //
+  //     dom.byId(tableDom).innerHTML = table;
+  //   });
+  //   navigationQueryTask.on("error", function (error) {
+  //     console.log("error: ", error);
+  //   });
+  //   navigationQueryTask.execute(navigationQuery);
+  // }
+
+  function navigationQuery(serviceUrl, id) {
     var url = "http://services.arcgis.com/WLhB60Nqwp4NnHz3/arcgis/rest/services/Navigation_EW/FeatureServer/0";
-    var tableDom = (serviceUrl === url) ? "tab-east-west" : "tab-west-east";
+    var fieldID = (serviceUrl === url) ? "OBJECTID = '" : "ID = '";
     var navigationQueryTask = new QueryTask(serviceUrl);
     var navigationQuery = new Query();
+    // dom.byId(tableDom).style.display = 'block';
     navigationQuery.outFields = ["*"];
     navigationQuery.returnGeometry = false;
     navigationQuery.multipatchOption = "xyFootprint";
-    navigationQuery.objectIds = [id];
-    // navigationQuery.where = "OBJECTID = '" + id + "'";
+    // navigationQuery.objectIds = [id];
+    navigationQuery.where = fieldID + id + "'";
+    navigationQueryTask.execute(navigationQuery);
     navigationQueryTask.on("complete", function (data) {
       console.log("data: ", data);
-
       var features = data.featureSet.features;
+      if (serviceUrl === url) {
+        topic.publish("ew:data-loaded", features);
+      }
+      else {
+        topic.publish("we:data-loaded", features);
+      }
+    });
+    navigationQueryTask.on("error", function (error) {
+      console.log("error: ", error);
+    });
+  }
+
+  function navigationDirections(features, tabDom) {
+
       var table = "<table cellspacing='0' cellpadding='10'><thead><tr>" +
         "<th>Images</th><th>Directions</th></tr></thead>";
       table += "<tbody>";
@@ -111,12 +184,7 @@ require([
 
       table += "</tbody></table>";
 
-      dom.byId(tableDom).innerHTML = table;
-    });
-    navigationQueryTask.on("error", function (error) {
-      console.log("error: ", error);
-    });
-    navigationQueryTask.execute(navigationQuery);
+    dom.byId(tabDom).innerHTML = table;
   }
 
   function init() {
@@ -151,9 +219,8 @@ require([
 
     var selectionSymbol = new SimpleLineSymbol().setColor(lines[id].color).setWidth(lines[id].width);
 
-
-    navigationDirections(navigationEWServiceUrl, id);
-    navigationDirections(navigationWEServiceUrl, id);
+    navigationQuery(navigationEWServiceUrl, id);
+    navigationQuery(navigationWEServiceUrl, id);
 
     search.startup();
     search.search(id).then(function (resp) {
